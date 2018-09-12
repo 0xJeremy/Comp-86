ff8db86a65ffddf5280a71937191411e1dd2567e
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-09-12 17:07:21 -0400

    Updated Interface

--- hw1/Main.java
+++ hw1/Main.java
@@ -6,10 +6,17 @@ public class Main
 	public static void main(String args[])
 	{
 		String hello = "Hello World!";
+		String[] hello_array = {"Hello 1", "Hello 2", "Hello 3", "Hello 4"};
 		System.out.println(hello);
 		frame window = new frame("Comp 86 – Homework 1", 600, 400);
+		// window.add_quit_button();
+		for(int i = 0; i < 4; i++)
+		{
+			window.add_print_button(hello_array[i], hello_array[i], 10, (i*40)+1);
+			window.add_checkbox(hello_array[i], 170, (i*40)+1);
+		}
+		window.add_combobox(hello_array, 330, 10);
 		window.add_quit_button();
-		window.add_print_button(hello, hello);
 		window.setVisible(true);
 	}
 
--- hw1/frame.java
+++ hw1/frame.java
@@ -2,6 +2,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -9,9 +11,13 @@ import javax.swing.SwingUtilities;
 public class frame extends JFrame
 {
 	private JPanel panel = new JPanel();
+	private int window_height;
+	private int window_width;
 
 	public frame(String window_name, int height, int width)
 	{
+		window_height = height;
+		window_width = width;
 		setTitle(window_name);
 		setSize(height, width);
 		setLocationRelativeTo(null);
@@ -23,7 +29,9 @@ public class frame extends JFrame
 	public void add_quit_button()
 	{
 		JButton quitButton = new JButton("Quit");
-		quitButton.setBounds(10, 10, 80, 30);
+		int button_x = window_width-80;
+		int button_y = window_height-10;
+		quitButton.setBounds(button_x, button_y, 80, 30);
 		quitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				System.exit(0);
@@ -32,15 +40,41 @@ public class frame extends JFrame
 		panel.add(quitButton);
 	}
 
-	public void add_print_button(String button_text, String print_out)
+	public void add_print_button(String button_text, String print_out, int x, int y)
 	{
 		JButton button = new JButton(button_text);
-		button.setBounds(100, 100, 200, 30);
+		button.setBounds(x, y, 150, 30);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				System.out.println(print_out);
 			}
 		});
 		panel.add(button);
+	}
+
+	public void add_switchable_button(String button_text, String print_out, int x, int y)
+	{
+		JButton button = new JButton(button_text);
+		button.setBounds(x, y, 150, 30);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				System.out.println();
+			}
+		});
+		panel.add(button);
+	}
+
+	public void add_checkbox(String button_text, int x, int y)
+	{
+		JCheckBox box = new JCheckBox(button_text);
+		box.setBounds(x, y, 150, 30);
+		panel.add(box);
+	}
+
+	public void add_combobox(String[] button_text, int x, int y)
+	{
+		JComboBox<String> box = new JComboBox<String>(button_text);
+		box.setBounds(x, y, 150, 30);
+		panel.add(box);
 	}
 }
