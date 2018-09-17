c67262a52f3c57a286eb979c1df25818e1443dfa
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-09-17 13:47:42 -0400

    Reuploaded HW1

---
+++ hw1/Main.java
@@ -0,0 +1,29 @@
+/******************************
+* Jeremy Kanovsky
+* jeremy.kanovsky@tufts.edu
+* This is the main function for Homework 1. It creates a new window interface.
+******************************/
+
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		String hello = "Hello World!";
+		String[] hello_array = {"Hello 1", "Hello 2", "Hello 3", "Hello 4"};
+		System.out.println(hello);
+		frame window = new frame("Comp 86 – Homework 1", 650, 200);
+		// window.add_quit_button();
+		for(int i = 0; i < 4; i++)
+		{
+			window.add_print_button(hello_array[i], hello_array[i], 10, (i*40)+10);
+			window.add_checkbox(hello_array[i], hello_array[i], 170, (i*40)+10);
+			window.add_combobox(hello_array, 330, (i*40)+10);
+			window.add_switchable_button(hello_array, i, 490, (i*40)+10);
+		}
+		window.setVisible(true);
+	}
+
+}
