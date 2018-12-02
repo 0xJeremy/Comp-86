fe0544a372e01d049e9650f18e004ad88061b786
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-12-02 12:37:27 -0500

    Added Functionality

--- hw10/scripts.js
+++ hw10/scripts.js
@@ -39,7 +39,8 @@ function animate() {
 function text_updater() {
 	var fieldNameElement = document.getElementById("info");
 	fieldNameElement.textContent = "The number of enemy ships is: " + (cube_counter + sphere_counter) +
-									". There are " + cube_counter + " boats and " + sphere_counter + " planes.";
+									". There are " + cube_counter + " boats and " + sphere_counter + " planes."
+									+ " There is 1 enemy";
 }
 
 
@@ -51,7 +52,7 @@ function update()
 			var cube_name = "cube" + i;
 			var cube = scene.getObjectByName( cube_name );
 			cube.material.color.setHex(0x9999ff);
-			cube.position.set(i*2, i*2, i*2);
+			cube.translateY(1);
 		}
 	}
 	else if(keyboard.pressed("s"))
@@ -60,7 +61,25 @@ function update()
 			var cube_name = "cube" + i;
 			var cube = scene.getObjectByName( cube_name );
 			cube.material.color.setHex(0x000088);
-			cube.position.set(i, i, i);
+			cube.translateY(-1);
+		}
+	}
+	else if(keyboard.pressed("d"))
+	{
+		for(var i = 0; i < cube_counter; i++) {
+			var cube_name = "cube" + i;
+			var cube = scene.getObjectByName( cube_name );
+			cube.material.color.setHex(0x000088);
+			cube.translateX(1);
+		}
+	}
+	else if(keyboard.pressed("a"))
+	{
+		for(var i = 0; i < cube_counter; i++) {
+			var cube_name = "cube" + i;
+			var cube = scene.getObjectByName( cube_name );
+			cube.material.color.setHex(0x000088);
+			cube.translateX(-1);
 		}
 	}
 	else if(keyboard.pressed("c"))
@@ -69,7 +88,12 @@ function update()
 		var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x000088 } );
 		var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
 		cube.name = "cube" + cube_counter;
-		cube.position.set(cube_counter, cube_counter, cube_counter);
+		if(cube_counter % 2 == 0) {
+			cube.position.set(cube_counter, cube_counter, cube_counter);
+		}
+		else {
+			cube.position.set(-cube_counter, cube_counter, cube_counter);
+		}
 		scene.add(cube);
 		cube_counter++;
 	}
@@ -85,7 +109,7 @@ function update()
 	else if(keyboard.pressed("p"))
 	{
 		var sphereGeometry = new THREE.SphereGeometry(1);
-		var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x864212 } );
+		var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xcc0000 } );
 		sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
 		sphere.position.set(1+sphere_counter, sphere_counter*2, -2);
 		scene.add(sphere);
@@ -131,7 +155,24 @@ function render() {
 }
 
 
-function makeSceneGraph () { 
+function makeSceneGraph () {
+
+	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
+	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+	scene.add(skyBox);
+
+	var tower = new Tower (new THREE.MeshPhongMaterial ({
+		color: "orange", shininess: 60 }))
+	tower.position.set (0, 0, -6)
+	tower.rotation.set (0, 45 * Math.PI/180, 0)
+	scene.add(tower)
+
+	generate_cubes()
+	generate_spheres()
+}
+
+function generate_cubes() {
 	for(var i = 0; i < 5; i++) {
 		var cubeGeometry = new THREE.CubeGeometry( 1, 1, 1 );
 		var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x000088 } );
@@ -140,31 +181,32 @@ function makeSceneGraph () {
 		cube.position.set(i,i,i);
 		scene.add(cube);
 		cube_counter++;
+		var cube2 = new THREE.Mesh( cubeGeometry, cubeMaterial );
+		cube2.name = "cube" + cube_counter;
+		cube2.position.set(-i,i,i);
+		scene.add(cube2);
+		cube_counter++;
 	}
+}
 
-	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
-	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
-	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-	scene.add(skyBox);
-
+function generate_spheres() {
 	for(var i = 0; i < 5; i++) {
 		var sphereGeometry = new THREE.SphereGeometry(1);
-		var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x864212 } );
+		var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xcc0000 } );
 		sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
 		sphere.position.set(1+i, i*2, -2);
 		scene.add(sphere);
 		sphere.name = "sphere" + sphere_counter;
 		sphere_counter++;
+		sphere2 = new THREE.Mesh( sphereGeometry, sphereMaterial );
+		sphere2.position.set(-(1+i), i*2, -2);
+		scene.add(sphere2);
+		sphere2.name = "sphere" + sphere_counter;
+		sphere_counter++;
 	}
-
-	var pyramid = new Pyramid (new THREE.MeshPhongMaterial ({
-		color: "green", shininess: 60 }))
-	pyramid.position.set (0, 0, -6)
-	pyramid.rotation.set (0, 45 * Math.PI/180, 0)
-	scene.add(pyramid)
 }
 
-class Pyramid extends THREE.Object3D {
+class Tower extends THREE.Object3D {
     constructor (material) {
 	super ()
 	var bot = new THREE.Mesh (
@@ -182,6 +224,16 @@ class Pyramid extends THREE.Object3D {
 		material)
 	top.position.set (0, 2 + 1/2, 0)
 	this.add(top)
+	var bot_mid = new THREE.Mesh (
+		new THREE.BoxGeometry (2, 2, 2),
+		material)
+	bot_mid.position.set(0, -8/2, 0)
+	this.add(bot_mid)
+	var bot_bot = new THREE.Mesh (
+		new THREE.BoxGeometry (1, 1, 1),
+		material)
+	bot_bot.position.set(0, -11/2, 0)
+	this.add(bot_bot)
     }
 }
 
